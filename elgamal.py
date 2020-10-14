5d9ab8e04a139c18c39d722eae4f8bc403f0f553
Feqzz <stian.onarheim@protonmail.com>
2020-10-14 16:18:53 +0200

    Started constructing blocks

--- elgamal.py
+++ elgamal.py
@@ -33,28 +33,69 @@ def generateLowestAcceptableGenerator(prime):
 
 	return -1
 
-def constructBlocks(str, prime):
+def constructBlocks(message, prime):
 	if prime < 131:
 		sys.exit("Your Primenumber is less than the minimum (131).")
 
 	retList = []
 
-	for i in str:
+	i = 0
+	while i < len(message):
+		#print("index: ", i)
 		tempList = []
+		tempValue = ord(message[i])
+		print("TempValue: ", tempValue)
+		for j, k in enumerate(message[(i+1):len(message)], 1):
+			concatenateValue = int(str(tempValue) + str(ord(k)))
 
-		if ord(i) < 300:
-			print(ord(i))
+			if concatenateValue > prime:
+				break
+			else:
+				tempValue = concatenateValue
+				print("adding to index: ", 1)
+				i += 1
+			
+			print(concatenateValue)
+
+		print("adding: ", tempValue)
+		retList.append(tempValue)
+
+		i += 1
+
+
+	print("Printing blocks list")
+	for i in retList:
+		print(i)
+
+	print("Printing orignal message")
+	for i in message:
+		print(ord(i))
+
+def compressMessage(message):
+	retStr = ""
+	for i in message:
+		retStr += str(ord(i) - 22)
+
+	print(retStr)
+	return retStr
+
+
+
+def elgamal():
+	print("")
+
 
 		
 
 def main():
 
-	stringMessage = "Heisann"
+	stringMessage = "HELLO"
+	#compressedMessage = compressMessage(stringMessage)
 
 
 
 
-	prime = 999983	
+	prime = 999983
 	q = prime - 1
 	generator = generateLowestAcceptableGenerator(prime)
 
